9f56116c19ecaf8986ea360cfd2e011086760658
岡井義宗 <yoshimune.okai@e-seikatsu.co.jp>
2024-02-12 22:21:48 +0900

    amplify init, codegen実行

---
+++ src/graphql/queries.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const singleTodo = /* GraphQL */ `
+  query SingleTodo($id: ID!) {
+    singleTodo(id: $id) {
+      id
+      title
+      description
+      __typename
+    }
+  }
+`;
+export const allTodos = /* GraphQL */ `
+  query AllTodos {
+    allTodos {
+      id
+      title
+      description
+      __typename
+    }
+  }
+`;
